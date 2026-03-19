1e4218f25a3ebd886ff18617841466b5a6f47b10
Lakshmithiru5 <rajalakshmithiru5@gmail.com>
2026-03-19 15:09:08 +0530

    Initial Commit

---
+++ src/main/java/com/rajalakshmiit/hello/App.java
@@ -0,0 +1,12 @@
+package com.rajalakshmiit.hello;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("Bye Bye!");
+	}
+}
